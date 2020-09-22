c2fab46230fb2e7d618cfb1cffa707b4a67b501f
Ishwari Kanase <ishwari.kanase@gmail.com>
2020-09-22 09:13:02 +0530

    remove is modified

--- src/app/components/que4/que4.component.ts
+++ src/app/components/que4/que4.component.ts
@@ -10,38 +10,44 @@ export class Que4Component implements OnInit {
   list = [];
   newTodoValue;
   selectedItem;
-  indexes=[];
+  checked:boolean=false;
+  indexes = [];
   constructor() { }
 
   ngOnInit() {
-    this.list = ["angular", "react","node"];
+    this.list = ["angular", "react", "node", "a", "b", "c", "d"];
   }
 
   add() {
-    if(this.newTodoValue!=null){
+    if (this.newTodoValue != null) {
       this.list.push(this.newTodoValue);
     }
-    else{
+    else {
       alert("please! enter something");
     }
   }
 
   selected(event) {
     this.selectedItem = event.target.value;
-    this.indexes.push(this.list.findIndex(x=>x==this.selectedItem));
-    this.indexes.sort();
-    this.indexes.reverse();
-    console.log(this.indexes);
+    this.checked=event.target.checked;
+    if (!this.indexes.includes(this.list.findIndex(x => x == this.selectedItem)) && this.checked) {
+      this.indexes.push(this.list.findIndex(x => x == this.selectedItem));
+    }
+    if (!this.checked && this.indexes.includes(this.list.findIndex(x => x == this.selectedItem))) {
+      this.indexes.splice(this.list.findIndex(x => x == this.selectedItem), 1);
+    }
   }
 
   remove() {
-    if(this.selectedItem!=null){
-      // this.list.splice(this.indexes.reduce(x=>x),this.indexes.length);
-      this.indexes.map(x=>{
-        console.log(this.list.splice(x,1));
-      })
+    if (this.indexes.length != 0 && this.checked) {
+      this.indexes.sort();
+      this.indexes.reverse();
+      this.indexes.map(x => {
+        this.list.splice(x, 1);
+      });
+      this.indexes=[];
     }
-    else{
+    if(this.indexes.length == 0 && !this.checked){
       alert("please select at least one item");
     }
   }
